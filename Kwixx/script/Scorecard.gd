04e9db0a1c23ac41c9c3f21ade80681cc1dbe631
Lonn <lonn.wolf031@gmail.com>
2021-04-04 20:24:39 +0200

    update

--- Kwixx/script/Scorecard.gd
+++ Kwixx/script/Scorecard.gd
@@ -25,19 +25,21 @@ func _ready():
 	for _container in _buttoncontainers:
 		containerSizesOrig[_container.get_name()] = _container.get_size()
 		containerPosOrig[_container.get_name()] = _container.get_position()
-	#print(str(containerPosOrig))
-	print("scorecard is of type: ", str(_scorecardTxture))
-
 	#untoggle all childs of button container
 	scale_reposition()
+	print("at start ", str(_scorecardTxture.get_size()))
 
 
 func scale_reposition():
 	var _scorecardSize = _scorecardTxture.get_size() #returns Vector2
+	print("get size: ", str(_scorecardTxture.get_size()))
 	for _container in _buttoncontainers:
 		var _newContainerXsize = _scorecardSize.x * containerSizesOrig[_container.get_name()].x / _scorecardTxtureSizeOrig.x
 		var _newContainerYsize = _scorecardSize.y * containerSizesOrig[_container.get_name()].y  / _scorecardTxtureSizeOrig.y
 		_container.set_size(Vector2(_newContainerXsize, _newContainerYsize))
+		#var _newContainerXsize = resize_values_by_ratio(target_size, image_size).x
+		#var _newContainerYsize resize_values_by_ratio(target_size, image_size).y
+		#_container.set_size()
 		var _newContainerPosX = containerPosOrig[_container.get_name()].x * _newContainerXsize / _buttonContainerSizeOrig.x 
 		var _newContainerPosY = containerPosOrig[_container.get_name()].y * _newContainerYsize / _buttonContainerSizeOrig.y 
 		_container.set_position(Vector2(_newContainerPosX,_newContainerPosY))
@@ -50,8 +52,21 @@ func scale_reposition():
 		elif _parent is Button:
 			_checkbox.set_size(Vector2(_parent.get_size().x, _parent.get_size().y))
 
-
-
+func resize_values_by_ratio(scorecard_orig_size, scorecard_new_size, image_size):
+	var resize_width
+	var resize_height
+	var scorecard_orig_ratio = scorecard_orig_size.y / scorecard_orig_size.x
+	var scorecard_scale = scorecard_new_size.y / scorecard_orig_size.y
+	# check ratios, compare, return right values
+	if scorecard_ratio > im_ratio:
+		# It must be fixed by width
+		resize_width = scorecard_size.x
+		resize_height = resize_width * im_ratio
+	else:
+		# Fixed by height
+		resize_height = scorecard_size.x
+		resize_width = resize_height / im_ratio
+	return Vector2(resize_width, resize_height)
 
 func _on_scorecard_resized():
 	scale_reposition()
@@ -60,6 +75,7 @@ func _on_scorecard_resized():
 func _button_pressed(which):
 	print("Button was pressed: ", which.get_name())
 	_toggle(which)
+	print("on click ", str(_scorecardTxture.get_size()))
 	
 func _toggle(sentby):
 	var _sentby = sentby.get_name()
